a29a753b91d74384d668da8c25ba6c8ec23365d8
Rakbow <rakbow@gmail.com>
2023-11-07 00:44:25 +0800

    新增枚举工具类

--- src/main/java/com/rakbow/kureakurusu/util/EnumHelper.java
+++ src/main/java/com/rakbow/kureakurusu/util/EnumHelper.java
@@ -1,6 +1,14 @@
 package com.rakbow.kureakurusu.util;
 
+import com.rakbow.kureakurusu.data.Attribute;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * @Project_name: kureakurusu
@@ -21,6 +29,38 @@ public class EnumHelper {
             e.printStackTrace();
             return "Error: " + e.getMessage();
         }
+    }
+
+    public static <T extends Enum<T>> List<Attribute<Integer>> getAttributeOptions(Class<T> clazz, String lang) {
+        List<Attribute<Integer>> attributes = new ArrayList<>();
+        for (T enumValue : clazz.getEnumConstants()) {
+            try {
+                Field valueField = clazz.getDeclaredField("value");
+                valueField.setAccessible(true);
+                Integer value = (Integer) valueField.get(enumValue);
+
+                // 获取 getLabelKey 方法
+                Method getLabelKeyMethod = clazz.getDeclaredMethod("getLabelKey");
+
+                // 调用 getLabelKey 方法
+                Object result = getLabelKeyMethod.invoke(enumValue);
+
+                // 将结果转换为字符串
+                String labelKey = result.toString();
+
+                String label = I18nHelper.getMessage(labelKey, lang);
+
+                Attribute<Integer> attribute = new Attribute<>(label, value);
+
+                attributes.add(attribute);
+
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                // handle exception
+            } catch (InvocationTargetException | NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return attributes;
     }
 
 }
