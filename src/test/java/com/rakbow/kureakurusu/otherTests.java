6100393ae1cafb5a8282d6a96d721e0af0bdf6c3
Jingzhen Wang <wangjingzhen@bugongsoft.com>
2024-01-26 16:20:32 +0800

    废弃music模块

--- src/test/java/com/rakbow/kureakurusu/otherTests.java
+++ src/test/java/com/rakbow/kureakurusu/otherTests.java
@@ -1,16 +1,11 @@
 package com.rakbow.kureakurusu;
 
-import com.rakbow.kureakurusu.dao.MusicMapper;
-import com.rakbow.kureakurusu.service.AlbumService;
-import com.rakbow.kureakurusu.service.MusicService;
 import com.rakbow.kureakurusu.util.file.QiniuImageUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.Resource;
 
 /**
  * @author Rakbow
@@ -21,57 +16,12 @@ import javax.annotation.Resource;
 @ContextConfiguration(classes = KureaKurusuApplication.class)
 public class otherTests {
 
-    @Resource
-    private AlbumService albumService;
-    @Resource
-    private MusicService musicService;
-    @Resource
-    private MusicMapper musicMapper;
-
 
     @Test
     public void tmpTest() {
         String url = "https://img.rakbow.com/album/6/f57fa8a51b594e30.jpg";
         System.out.println(QiniuImageUtil.getImageKeyByFullUrl(url));
     }
-
-    // @Test
-    // public void test1() {
-    //     List<Music> musicList = musicMapper.selectList(new LambdaQueryWrapper<Music>().eq(Music::getStatus, 1).orderByAsc(Music::getAlbumId));
-    //     List<Long> albumIds = musicList.stream().map(Music::getAlbumId).distinct().toList();
-    //     List<Episode> addData = new ArrayList<>();
-    //
-    //     Comparator<Music> comparator = Comparator
-    //             .comparing(Music::getDiscSerial)
-    //             .thenComparing(Music::getTrackSerial);
-    //
-    //     for (long id : albumIds) {
-    //         List<Music> tmpMusicList = musicList.stream().filter(music -> music.getAlbumId() == id).toList();
-    //
-    //         for (Music m : tmpMusicList) {
-    //             Episode ep = new Episode();
-    //
-    //             ep.setTitle(m.getName());
-    //             ep.setTitleEn(m.getNameEn());
-    //             ep.setDuration(getDuration(m.getAudioLength()));
-    //             ep.setDiscNum(m.getDiscSerial());
-    //             ep.setSerial(Integer.parseInt(m.getTrackSerial()));
-    //             ep.setRelatedId(id);
-    //
-    //             ep.setAddedTime(m.getAddedTime());
-    //             ep.setEditedTime(m.getEditedTime());
-    //
-    //             addData.add(ep);
-    //         }
-    //     }
-    //
-    //     SqlSessionFactory sqlSessionFactory = SpringUtil.getBean("sqlSessionFactory");
-    //     //批量新增
-    //     MybatisBatch.Method<Episode> method = new MybatisBatch.Method<>(EpisodeMapper.class);
-    //     MybatisBatch<Episode> batchInsert = new MybatisBatch<>(sqlSessionFactory, addData);
-    //     batchInsert.execute(method.insert());
-    //
-    // }
 
     public static int getDuration(String time) {
         String[] parts = time.split(":");
@@ -89,6 +39,8 @@ public class otherTests {
             // 处理格式错误的情况
             System.err.println("Invalid time format");
         }
+
+        var a = 1;
 
         return 0; // 或者返回合适的默认值
     }
