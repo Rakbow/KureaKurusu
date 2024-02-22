53b9407606e63fb6f87726a35be02cc15c2a20fe
Rakbow <rakbow@gmail.com>
2024-02-22 15:28:40 +0800

    优化person实体类结构

--- src/main/java/com/rakbow/kureakurusu/data/entity/Person.java
+++ src/main/java/com/rakbow/kureakurusu/data/entity/Person.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.rakbow.kureakurusu.data.Gender;
 import com.rakbow.kureakurusu.data.Link;
+import com.rakbow.kureakurusu.data.entity.common.MetaEntity;
 import com.rakbow.kureakurusu.data.image.Image;
 import com.rakbow.kureakurusu.util.common.DateHelper;
 import com.rakbow.kureakurusu.util.handler.ImageHandler;
@@ -12,6 +13,9 @@ import com.rakbow.kureakurusu.util.handler.StrListHandler;
 import lombok.Data;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +24,11 @@ import java.util.List;
  * @author Rakbow
  * @since 2023-10-06 3:59
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
+@ToString(callSuper = true)
 @TableName(value = "person", autoResultMap = true)
-public class Person {
+public class Person extends MetaEntity {
 
     private Long id; //主键
     @NotBlank(message = "{entity.crud.name.required_field}")
@@ -34,16 +40,9 @@ public class Person {
     private String cover; //头像url
     private Gender gender; //性别 0-未知 1-男 2-女
     private String birthDate; //生日
-    private String detail; //简介
     @TableField(typeHandler = LinkHandler.class)
     private List<Link> links; //链接 json数组
-    @TableField(typeHandler = ImageHandler.class)
-    private List<Image> images; //图片合集 json数组
-    private String remark; //备注
     private String info; //其他信息 json对象
-    private Timestamp addedTime; //录入时间
-    private Timestamp editedTime; //编辑时间
-    private int status; //状态
 
     public Person() {
         id = 0L;
@@ -54,14 +53,14 @@ public class Person {
         cover = "";
         gender = Gender.UNKNOWN;
         birthDate = "";
-        detail = "";
         links = new ArrayList<>();
-        images = new ArrayList<>();
-        remark = "";
         info = "{}";
-        addedTime = DateHelper.now();
-        editedTime = DateHelper.now();
-        status = 1;
+        setDetail("");
+        setRemark("");
+        setImages(new ArrayList<>());
+        setAddedTime(DateHelper.now());
+        setEditedTime(DateHelper.now());
+        setStatus(1);
     }
 
 }
