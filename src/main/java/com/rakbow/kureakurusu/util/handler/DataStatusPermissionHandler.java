888ba9480deacde06abe9fe483196e8e13375091
Rakbow <rakbow@gmail.com>
2024-04-03 20:00:12 +0800

    update data permission handler

--- src/main/java/com/rakbow/kureakurusu/util/handler/DataStatusPermissionHandler.java
+++ src/main/java/com/rakbow/kureakurusu/util/handler/DataStatusPermissionHandler.java
@@ -1,11 +1,9 @@
 package com.rakbow.kureakurusu.util.handler;
 
 import com.baomidou.mybatisplus.extension.plugins.handler.DataPermissionHandler;
-import com.rakbow.kureakurusu.data.emun.UserAuthority;
-import com.rakbow.kureakurusu.data.entity.User;
 import com.rakbow.kureakurusu.interceptor.AuthorityInterceptor;
 import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.StringValue;
+import net.sf.jsqlparser.expression.LongValue;
 import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
 import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
 import net.sf.jsqlparser.schema.Column;
@@ -21,15 +19,13 @@ public class DataStatusPermissionHandler implements DataPermissionHandler {
 
     @Override
     public Expression getSqlSegment(Expression where, String mappedStatementId) {
+
         if(StringUtils.equals(mappedStatementId, "com.rakbow.kureakurusu.dao.UserMapper.selectById")) return where;
 
-        User user = AuthorityInterceptor.getCurrentUser();
-        if (user == null) return where;
-
-        if (UserAuthority.isJunior(user)) {
+        if (AuthorityInterceptor.isJunior()) {
             return where;
         } else {
-            EqualsTo equalsTo = new EqualsTo(new Column("status"), new StringValue("1"));
+            EqualsTo equalsTo = new EqualsTo(new Column("status"), new LongValue("1"));
             if (where == null) {
                 // 如果原来没有where条件, 就添加一个where条件
                 return equalsTo;
