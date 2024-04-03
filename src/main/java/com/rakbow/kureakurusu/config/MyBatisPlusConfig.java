d7808eba92ec9d2b27c197012979315cc6f4ce31
Rakbow <rakbow@gmail.com>
2024-04-03 19:41:55 +0800

    add mybatisPlus data permission handler update mybatisPlus config

--- src/main/java/com/rakbow/kureakurusu/config/MyBatisPlusConfig.java
+++ src/main/java/com/rakbow/kureakurusu/config/MyBatisPlusConfig.java
@@ -1,16 +1,14 @@
 package com.rakbow.kureakurusu.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.autoconfigure.MybatisPlusProperties;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.DataPermissionInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.DialectFactory;
+import com.rakbow.kureakurusu.util.handler.DataStatusPermissionHandler;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.ResourceLoader;
-
-import javax.sql.DataSource;
 
 /**
  * @author Rakbow
@@ -18,20 +16,19 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class MyBatisPlusConfig {
-
     @Resource
-    private DataSource dataSource;
-    @Resource
-    private MybatisPlusProperties properties;
-    @Resource
-    private ResourceLoader resourceLoader = new DefaultResourceLoader();
-
-    //MyBatisPlus拦截器（用于分页）
+    private DataStatusPermissionHandler dataPermissionHandler;
     @Bean
     public MybatisPlusInterceptor paginationInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+
+        //添加MySQL的数据权限拦截器
+        interceptor.addInnerInterceptor(new DataPermissionInterceptor(dataPermissionHandler));
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        paginationInnerInterceptor.setDialect(DialectFactory.getDialect(DbType.MYSQL));
         //添加MySQL的分页拦截器
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
         return interceptor;
     }
 
---
+++ src/main/java/com/rakbow/kureakurusu/util/handler/DataStatusPermissionHandler.java
@@ -0,0 +1,43 @@
+package com.rakbow.kureakurusu.util.handler;
+
+import com.baomidou.mybatisplus.extension.plugins.handler.DataPermissionHandler;
+import com.rakbow.kureakurusu.data.emun.UserAuthority;
+import com.rakbow.kureakurusu.data.entity.User;
+import com.rakbow.kureakurusu.interceptor.AuthorityInterceptor;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.StringValue;
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
+import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
+import net.sf.jsqlparser.schema.Column;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Rakbow
+ * @since 2024/4/3 18:19
+ */
+@Component
+public class DataStatusPermissionHandler implements DataPermissionHandler {
+
+    @Override
+    public Expression getSqlSegment(Expression where, String mappedStatementId) {
+        if(StringUtils.equals(mappedStatementId, "com.rakbow.kureakurusu.dao.UserMapper.selectById")) return where;
+
+        User user = AuthorityInterceptor.getCurrentUser();
+        if (user == null) return where;
+
+        if (UserAuthority.isJunior(user)) {
+            return where;
+        } else {
+            EqualsTo equalsTo = new EqualsTo(new Column("status"), new StringValue("1"));
+            if (where == null) {
+                // 如果原来没有where条件, 就添加一个where条件
+                return equalsTo;
+            } else {
+                return new AndExpression(where, equalsTo);
+            }
+
+        }
+    }
+
+}
