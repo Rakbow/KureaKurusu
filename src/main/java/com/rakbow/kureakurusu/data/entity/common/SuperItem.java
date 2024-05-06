e084ddedcc6ca43d3057ad04ada3f2ae9a25cd9a
Rakbow <rakbow@gmail.com>
2024-05-06 12:56:02 +0800

    little update

--- src/main/java/com/rakbow/kureakurusu/data/entity/common/SuperItem.java
+++ src/main/java/com/rakbow/kureakurusu/data/entity/common/SuperItem.java
@@ -7,7 +7,6 @@ import com.rakbow.kureakurusu.data.image.Image;
 import com.rakbow.kureakurusu.util.common.DateHelper;
 import com.rakbow.kureakurusu.util.handler.ImageHandler;
 import com.rakbow.kureakurusu.util.jackson.BooleanToIntDeserializer;
-import io.github.linpeilie.annotations.AutoMappers;
 import io.github.linpeilie.annotations.AutoMapping;
 import lombok.Data;
 
@@ -26,15 +25,13 @@ public abstract class SuperItem {
     private List<Image> images;//图片列表
     private String detail;//描述
     private String remark;//备注
+    @AutoMapping(qualifiedByName = "getVOTime")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateHelper.DATE_TIME_FORMAT, timezone="GMT+8")
     private Timestamp addedTime;//数据新增时间
+    @AutoMapping(qualifiedByName = "getVOTime")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateHelper.DATE_TIME_FORMAT, timezone="GMT+8")
     private Timestamp editedTime;//数据更新时间
     @JsonDeserialize(using = BooleanToIntDeserializer.class)
     private Boolean status;//激活状态
-
-    public void updateEditedTime() {
-        this.editedTime = DateHelper.now();
-    }
 
 }
