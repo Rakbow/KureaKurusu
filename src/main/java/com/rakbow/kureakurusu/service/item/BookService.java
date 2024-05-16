1bb8b71934be7cbfa898c3cb9389cb60b6e395d8
Rakbow <rakbow@gmail.com>
2024-05-16 11:18:31 +0800

    optimize api: covert to isbn10 covert to isbn13

--- src/main/java/com/rakbow/kureakurusu/service/item/BookService.java
+++ src/main/java/com/rakbow/kureakurusu/service/item/BookService.java
@@ -3,11 +3,11 @@ package com.rakbow.kureakurusu.service.item;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.rakbow.kureakurusu.dao.ItemBookMapper;
 import com.rakbow.kureakurusu.data.entity.ItemBook;
-import com.rakbow.kureakurusu.toolkit.BookUtil;
 import com.rakbow.kureakurusu.toolkit.I18nHelper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.routines.ISBNValidator;
 import org.springframework.stereotype.Service;
 
 /**
@@ -28,16 +28,25 @@ public class BookService extends ServiceImpl<ItemBookMapper, ItemBook> {
     @SneakyThrows
     public String getISBN(String label, String isbn) {
 
+        ISBNValidator validator = new ISBNValidator();
         isbn = isbn.replaceAll("-", "");
 
         if (StringUtils.equals(label, "isbn13")) {
-            if (isbn.length() != 10)
+            if (validator.isValidISBN10(isbn))
                 throw new Exception(I18nHelper.getMessage("book.crud.isbn10.invalid"));
-            return BookUtil.getISBN13(isbn);
+            return validator.convertToISBN13(isbn);
         } else if (StringUtils.equals(label, "isbn10")) {
-            if (isbn.length() != 13)
+            if (validator.isValidISBN13(isbn))
                 throw new Exception(I18nHelper.getMessage("book.crud.isbn13.invalid"));
-            return BookUtil.getISBN10(isbn);
+            String isbn10Base = isbn.substring(3, 12);
+            int sum = 0;
+            for (int i = 0; i < 9; i++) {
+                sum += (10 - i) * (isbn10Base.charAt(i) - '0');
+            }
+            int checkDigit = 11 - (sum % 11);
+            String checkDigitStr = (checkDigit == 10) ? "X" : (checkDigit == 11) ? "0" : String.valueOf(checkDigit);
+
+            return isbn10Base + checkDigitStr;
         }
         return null;
     }
