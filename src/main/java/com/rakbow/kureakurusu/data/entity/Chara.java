2332d5f950a2dd8c6c3940ece6d052c69b2cad8f
Rakbow <rakbow@gmail.com>
2024-12-28 02:57:34 +0800

    update character part

--- src/main/java/com/rakbow/kureakurusu/data/entity/Chara.java
+++ src/main/java/com/rakbow/kureakurusu/data/entity/Chara.java
@@ -6,10 +6,12 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.rakbow.kureakurusu.data.emun.Gender;
+import com.rakbow.kureakurusu.data.entity.common.MetaEntity;
 import com.rakbow.kureakurusu.toolkit.DateHelper;
 import com.rakbow.kureakurusu.toolkit.handler.StrListHandler;
 import com.rakbow.kureakurusu.toolkit.jackson.BooleanToIntDeserializer;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
@@ -20,32 +22,24 @@ import java.util.List;
  * @since 2024/6/22 12:44
  */
 @Data
-@TableName(value = "character", autoResultMap = true)
+@EqualsAndHashCode(callSuper = true)
+@TableName(value = "chara", autoResultMap = true)
 @NoArgsConstructor
-public class Chara {
+public class Chara extends MetaEntity {
 
     private Long id;
     private String name;
+    private String nameZh;
     private String nameEn;
     @TableField(typeHandler = StrListHandler.class)
     private List<String> aliases;
     private Gender gender;
     private String birthday;
-    private String detail;
     @TableField(typeHandler = StrListHandler.class)
     private List<String> links;
 
-    private String favorites;
-    private String hates;
-    private String bloodGroup;
-
-    private String remark;
-    @TableField(updateStrategy = FieldStrategy.NEVER)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateHelper.DATE_TIME_FORMAT, timezone="GMT+8")
-    private Timestamp addedTime = DateHelper.now();
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateHelper.DATE_TIME_FORMAT, timezone="GMT+8")
-    private Timestamp editedTime = DateHelper.now();
-    @JsonDeserialize(using = BooleanToIntDeserializer.class)
-    private Boolean status;
+    // private String favorites;
+    // private String hates;
+    // private String bloodGroup;
 
 }
