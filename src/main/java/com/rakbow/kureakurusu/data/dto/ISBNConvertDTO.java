f5b642d46551b59e05834dd3181cfe7d3de40c83
Rakbow <rakbow@gmail.com>
2025-07-03 20:52:06 +0800

    fix bug about isbn10 convert to isbn13

---
+++ src/main/java/com/rakbow/kureakurusu/data/dto/ISBNConvertDTO.java
@@ -0,0 +1,14 @@
+package com.rakbow.kureakurusu.data.dto;
+
+import lombok.Data;
+
+/**
+ * @author Rakbow
+ * @since 2025/7/3 20:20
+ */
+@Data
+public class ISBNConvertDTO {
+
+    private String isbn10;
+
+}
